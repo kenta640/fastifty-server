9c1187da64f51a9fb59dc9e50b80cc3ef9d34270
kenta640 <kenta0803d@gmail.com>
2022-04-05 16:29:23 +1200

    server.js improved

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const fastify = require('./app')
 
 fastify.listen(
-  process.env.SERVER_PORT, 
+  process.env.PORT, 
   '0.0.0.0', (err, address) =>{
     if (err) throw err
     fastify.log.info(`server listening on ${address}`)
