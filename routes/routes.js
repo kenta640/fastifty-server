f053c14905bb3c70a0f7319a4e95746887b7dcd4
kenta640 <kenta0803d@gmail.com>
2022-04-10 10:55:27 +1200

    routing fixed

--- routes/routes.js
+++ routes/routes.js
@@ -7,8 +7,8 @@ async function routes (fastify, options) {
     })
     fastify.get('/user', user.getUserList);
     fastify.get('/user/:id', user.getUserDetail);
-    fastify.get('/novel', user.getUserList);
-    fastify.get('/novel/:id', user.getUserDetail);
+    fastify.get('/novel', novel.getNovelList);
+    fastify.get('/novel/:id', novel.getNovelDetail);
 }
 
 module.exports = routes;
