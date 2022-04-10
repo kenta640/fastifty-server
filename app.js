647ad3246ebcad7d137318e9451de8dc8dc51354
kenta640 <kenta0803d@gmail.com>
2022-04-10 12:26:47 +1200

    Cors added with fastify-cors

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const fastify = require('fastify')()
 const routes = require('./routes/routes')
-fastify.register(routes);
+const fastifyCors = require('fastify-cors')
+//var allowlist = ['http://localhost:3000', 'https://beast-novel.vercel.app/']
+
+fastify
+.register(routes)
+.register(require("fastify-cors"), {
+    origin: true
+});
 module.exports = fastify
