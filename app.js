249524a7691415722ef905d15a0967b0ed0af899
kenta640 <kenta0803d@gmail.com>
2022-04-10 14:20:14 +1200

    Cors configured

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
 const fastify = require('fastify')()
 const routes = require('./routes/routes')
-const fastifyCors = require('fastify-cors')
+const cors = require('fastify-cors')
 //var allowlist = ['http://localhost:3000', 'https://beast-novel.vercel.app/']
 
 fastify
 .register(routes)
-.register(require("fastify-cors"), {
-    origin: true
-});
+.register(cors);
 
-fastify.all("/*", (req, res) => {
-    console.log(req);
-    console.log(req.headers);
-    const testValue = req.headers["x-test-value"] || "";
-    const body = req.body || "";
-    res
-      .type("text/plain; charset=utf-8")
-      .send(
-        `${Date.now()} ${req.raw.method} ${
-          req.raw.originalUrl
-        } ${body} ${testValue} `
-      );
-  });
 module.exports = fastify
