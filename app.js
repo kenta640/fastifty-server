2a54dc804ef4638b1724478e05cc51f1d9e86a56
kenta640 <kenta0803d@gmail.com>
2022-04-10 12:50:55 +1200

    fastify-cors configured

--- app.js
+++ app.js
@@ -8,4 +8,18 @@ fastify
 .register(require("fastify-cors"), {
     origin: true
 });
+
+fastify.all("/*", (req, res) => {
+    console.log(req);
+    console.log(req.headers);
+    const testValue = req.headers["x-test-value"] || "";
+    const body = req.body || "";
+    res
+      .type("text/plain; charset=utf-8")
+      .send(
+        `${Date.now()} ${req.raw.method} ${
+          req.raw.originalUrl
+        } ${body} ${testValue} `
+      );
+  });
 module.exports = fastify
